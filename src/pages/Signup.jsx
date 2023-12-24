cffd89f5a67c5c44cce43ebc41344351eec16471
BhupendraLute <bhupendralute1234@gmail.com>
2023-12-24 23:16:43 +0530

    build some pages

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Signup as SignupCompoenent } from '../components'
+
+function Signup() {
+  return (
+    <div className='py-8'>
+        <SignupCompoenent />
+    </div>
+  )
+}
+
+export default Signup
